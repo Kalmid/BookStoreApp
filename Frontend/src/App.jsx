8cdcb6ce92b02a9c9539f8b53ccabd2f3d9dcb34
Kalmid <kalmid.98@gmail.com>
2024-07-23 21:39:37 +0530

    Update files and create new book file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 function App() {
 
