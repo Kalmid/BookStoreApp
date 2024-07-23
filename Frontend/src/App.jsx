2fe88a4d221e9292c6227c007a7fd07f8254669e
Kalmid <kalmid.98@gmail.com>
2024-07-23 21:17:17 +0530

    Update the files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import Footer from './components/footer';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <Navbar/>
     <Banner/>
+    <Footer/>
     </>
   );
 }
