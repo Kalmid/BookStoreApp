67c023c33ade4374b1945dbf087f5b6ea77b733a
Kalmid <kalmid.98@gmail.com>
2024-07-24 18:36:22 +0530

    Update the file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './home/Home';
+import Course from './components/Course';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Home/>
+    <Course/>
     </>
   );
 }
