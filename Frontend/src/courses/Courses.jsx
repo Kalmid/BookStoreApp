ed118fea544d5ff4b8d9fe2159567a0a730a1989
Kalmid <kalmid.98@gmail.com>
2024-07-24 20:51:44 +0530

    Update the file

--- Frontend/src/courses/Courses.jsx
+++ Frontend/src/courses/Courses.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Course from '../components/Course'
-import Footer from '../components/footer'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Course from '../components/Course';
+import Footer from '../components/footer';
 
-export default function Courses() {
-  return (
-    <>
-    <Navbar/>
-    <Course/>
-    <Footer/>
-    </>
-  )
+function Courses() {
+    return (
+        <>
+            <Navbar />
+            <div className="min-h-screen">
+                <Course />
+            </div>
+            <Footer />
+        </>
+    )
 }
+
+export default Courses;
