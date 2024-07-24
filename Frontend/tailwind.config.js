9e44e9d07bcbd87ae719c929a7b4839749f46064
Kalmid <kalmid.98@gmail.com>
2024-07-24 21:46:40 +0530

    Update the main file

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js,jsx,tsx}"],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
