17c8b0ab71439a2ddb671d759ed104a66e9ac8e7
Kalmid <kalmid.98@gmail.com>
2024-07-24 20:21:28 +0530

    Update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Home from './home/Home';
-import Course from './components/Course';
+import Courses from './courses/Courses';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
-  
+
   return (
     <>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/course" element={<Course/>}/>
+      <Route path="/course" element={<Courses/>}/>
     </Routes>
     </>
   );
