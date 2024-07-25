e03f989bcd9a927d65bd10f5fb40a58f690d8fd6
Kalmid <kalmid.98@gmail.com>
2024-07-25 21:37:31 +0530

    Update

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 function Login() {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = userForm();
+
     return (
         <div>
             <dialog id="my_modal_3" className="modal">
@@ -13,21 +20,27 @@ function Login() {
                     <h3 className="font-bold text-lg">Login</h3>
                     <div className="mt-4 space-y-2">
                         <span>Email</span>
-                        <br/>
+                        <br />
                         <input
-                        type="email"
-                        placeholder="Enter your email"
-                        className="w-80 px-3 py-1 border rounded-md outline-none"
+                            type="email"
+                            placeholder="Enter your email"
+                            className="w-80 px-3 py-1 border rounded-md outline-none"
+                            {...register("email", { required: true })}
                         />
+                        <br/>
+                        {errors.email && <span className="text-sm text-red-500">This field is requied</span>}
                     </div>
                     <div className="mt-4 space-y-2">
                         <span>Password</span>
-                        <br/>
+                        <br />
                         <input
-                        type="text"
-                        placeholder="Enter your Password"
-                        className="w-80 px-3 py-1 border rounded-md outline-none"
+                            type="password"
+                            placeholder="Enter your Password"
+                            className="w-80 px-3 py-1 border rounded-md outline-none"
+                            {...register("password", { required: true })}
                         />
+                        <br/>
+                        {errors.password && <span className="text-sm text-red-500">This field is requied</span>}
                     </div>
                     <div className="flex justify-around mt-4">
                         <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
