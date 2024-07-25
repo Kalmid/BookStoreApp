4976388f19158fa0849933605eefac23a592d45f
Kalmid <kalmid.98@gmail.com>
2024-07-25 20:13:26 +0530

    Update the frontend

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -54,8 +54,8 @@ function Navbar() {
         <>
             <div
                 className={` max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-slate-800 dark:text-white fixed top-0 left-0 right-0 z-50 ${sticky
-                        ? "sticky-navbar shadow-md bg-base-200 dark:bg-slate-700 dark:text-white duration-300 transition-all ease-in-out"
-                        : ""
+                    ? "sticky-navbar shadow-md bg-base-200 dark:bg-slate-700 dark:text-white duration-300 transition-all ease-in-out"
+                    : ""
                     }`}
             >
                 <div className="navbar">
@@ -135,12 +135,15 @@ function Navbar() {
                             </svg>
                         </label>
                         <div className="">
-                            <a className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer"
-                            onClick={() => document.getElementById("my_modal_3").showModal()}
+                            <a
+                                className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer"
+                                onClick={() =>
+                                    document.getElementById("my_modal_3").showModal()
+                                }
                             >
                                 Login
-                                </a>
-                                <Login/>
+                            </a>
+                            <Login />
                         </div>
                     </div>
                 </div>
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Login from './Login';
 
 function Signup() {
     return (
@@ -19,7 +20,7 @@ function Signup() {
                         <br/>
                         <input
                             type="text"
-                            placeholder="Enter your Name"
+                            placeholder="Enter your fullname"
                             className="w-80 px-3 py-1 border rounded-md outline-none"
                         />
                     </div>
@@ -45,13 +46,17 @@ function Signup() {
                         <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
                             SignUp
                         </button>
-                        <p>
+                        <p className="text-xl">
                             Have Account?{" "}
-                            <Link
+                            <button
                                 to="/"
-                                className="underline text-blue-500 cursor-pointer">
+                                className="underline text-blue-500 cursor-pointer"
+                                onClick={() => 
+                                    document.getElementById("my_modal_3").showModal()
+                                }>
                                 Login
-                            </Link>{" "}
+                            </button>{" "}
+                            <Login/>
                         </p>
                     </div>
                 </div>
